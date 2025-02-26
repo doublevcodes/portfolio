52963e69b36f5ba1019aeac707550af45a63e4fd
Vivaan Verma <vivaan.verma@gmail.com>
2025-02-26 20:24:31 +0000

    fix apos

--- app/blog/(posts)/qc-regulation/page.tsx
+++ app/blog/(posts)/qc-regulation/page.tsx
@@ -62,7 +62,7 @@ export default function Post({}) {
         of the NQI Act 2018. The current landscape of legislation in the USA has both positives and negatives. It is clear that the American government
         recognises the need to invest in QC and develop it further - and there are laws focusing on this; however, where America falls short is regulation.
         Regulating government agencies is a good first step, but with an ever-growing threat to encrypted communication, all internet transmission
-        could be compromised. It is, however, true that America doesn't exclusively use the internet, and for secure global communication, there needs to be global
+        could be compromised. It is, however, true that America doesn&apos;t exclusively use the internet, and for secure global communication, there needs to be global
         coordination.
       </p>
       <h3 className="mt-2 font-bold">European Union</h3>
@@ -72,12 +72,12 @@ export default function Post({}) {
         confirmation hearing to be appointed this role, Virkkunen highlighted that even though Europe has access to world-class QIS researchers and facilities
         the lack of international coordination causes the market to remain fragmented, and consequently, she proposed a comprehensive and unifying quantum
         strategy to be adopted by EU member states. She seeks to position a unified Europe as a global leader in the field of quantum innovation. The
-        European Declaration on Quantum Technologies introduced in December 2023, a year before Virkunnen's confirmation hearing, has already laid the
+        European Declaration on Quantum Technologies introduced in December 2023, a year before Virkunnen&apos;s confirmation hearing, has already laid the
         groundwork for her vision, as its key objectives are to foster a world-class quantum ecosystem, by aligning European, national and regional research
         and development efforts. Going back even further, in 2019, the EU launched the European Quantum Communication Infrastructure (EuroQCI) Initiative. The
         idea is for the EuroQCI to be a secure quantum communication infrastructure spanning the whole EU. This will be done in collaboration with all 27 member
         states and the ESA, where a terrestial segment will be setup linking locations at a national level, as well as a space segment using satellites. The
-        EuroQCI is intended to be a part of IRIS2, the EU's planned secure space-based communication system. It is evident that the EU lacks any regulation over
+        EuroQCI is intended to be a part of IRIS2, the EU&apos;s planned secure space-based communication system. It is evident that the EU lacks any regulation over
         quantum computing and its applications, especially in terms of PQC. The only mention of PQC seems to be in a mere recommendation published by the European
         Commission in April 2024 and an announcement from 18 out of 27 member states that &quot;preparing for the quantum threat should be considered an integral
         aspect of cybersecurity risk management&quot; but with no concrete steps laid out - this is simply a call for action. Once again, the need to invest in the
@@ -87,17 +87,17 @@ export default function Post({}) {
       <h3 className="mt-2 font-bold">United Kingdom</h3>
       <p className="tracking-wide">
         Not much has been said in terms of PQC implementation by the UK government. The National Cyber Security Council has published a whitepaper on the topic, and also
-        addressed it in their 2024 Annual Review. In this annual review, the NCSC acknowledges the threat that quantum computers pose to cryptography, accepting that the UK's
+        addressed it in their 2024 Annual Review. In this annual review, the NCSC acknowledges the threat that quantum computers pose to cryptography, accepting that the UK&apos;s
         critical national infrastructure needs to be protected. Essentially, the NCSC says that their main focus right now is to raise understanding and set examples of best
         practice by working with a small group of PQC consultancies until March 2025. After this, the NCSC plans to evaluate how they can best provide support and intervention
         to organisations in adopting PQC. Once again, there is a lack of stringent measures, but it is worth mentioning that the NCSC annual review succinctly highlights that
-        implement PQC isn't something that can be done quickly - this will take many companies multiple investment cycles and a few years at least; arguably, this provides even
+        implement PQC isn&apos;t something that can be done quickly - this will take many companies multiple investment cycles and a few years at least; arguably, this provides even
         more reason to mandate the shift to PQC as soon as possible.
       </p>
       <h2 className="mt-2 font-bold text-2xl">The Basis of Regulation</h2>
       <p className="tracking-wide">
-        Obviously, this won't happen overnight, but there are a few pillars that regulations and laws about PQC would do well to cover. One of the largest risks when it comes to
-        regulating technology like this is to restrict the wrong thing: legislation should affect the application of the technology, and not the technology itself. Here's what the
+        Obviously, this won&apos;t happen overnight, but there are a few pillars that regulations and laws about PQC would do well to cover. One of the largest risks when it comes to
+        regulating technology like this is to restrict the wrong thing: legislation should affect the application of the technology, and not the technology itself. Here&apos;s what the
         rules should do:
       </p>
       <p  className="tracking-wide">
@@ -125,11 +125,11 @@ export default function Post({}) {
         A lack of regulation can lead and inadvertently has led to some competition. Two key players in this are the USA and China, and the race for quantum dominance could be compared to
         the Cold War arms race, where analagous to the race for nuclear weapons, the battle is for development in quantum computing, encryption and communications. Both countries have made
         progress: China heavily invested in research through state funding and military-backed projects to develop the Micius Satellite (2016) which is the first quantum communication
-        satellite, enabling unhackable quantum key distribution (QKD) over thousands of kilometres. China has also built the world's largest quantum-secured communication network, linking
+        satellite, enabling unhackable quantum key distribution (QKD) over thousands of kilometres. China has also built the world&apos;s largest quantum-secured communication network, linking
         the cities of Beijing and Shanghai with QKD. In fact, the Chinese government has pledged over $15 billion for quantum research, dwarfing US federal funding. Conversely, the USA, which
-        we've already discussed in much detail remains a global leader in quantum hardware due to the presence of companies such as Google, Microsoft and IBM. It would be naive not to expect
+        we&apos;ve already discussed in much detail remains a global leader in quantum hardware due to the presence of companies such as Google, Microsoft and IBM. It would be naive not to expect
         that there are classified projects being run by the DARPA, NSA and other federal agencies where quantum computing is being applied to cybersecurity and the military, but nothing major
-        has been made public as of yet. As of now, no bad blood has emerged from these developments specifically, but the risk will always exist as long as collaboration isn't insisted upon.
+        has been made public as of yet. As of now, no bad blood has emerged from these developments specifically, but the risk will always exist as long as collaboration isn&apos;t insisted upon.
         <br />
         <br />
         Therefore, we see how the lack of international quantum regulations could easily lead to countries hoarding breakthroughs instead of collaborating on secure quantum tech. The domino effect
